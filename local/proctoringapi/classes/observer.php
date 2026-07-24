7e8739e52a3105fe7a249f54003ceddb5ee2663c
hemanth0672 <hemanth.kumar@beyondkey.com>
2026-07-24 16:34:04 +0530

    Insert the data into table properly

--- local/proctoringapi/classes/observer.php
+++ local/proctoringapi/classes/observer.php
@@ -33,44 +33,44 @@ class observer {
         );
     }*/
 
-public static function attempt_submitted(\mod_quiz\event\attempt_submitted $event) {
-    global $CFG, $DB;
-    require_once($CFG->libdir . '/filelib.php');
- 
-    $cmid = $event->contextinstanceid;
- 
-    $curl = new \curl();
-    $curl->setopt([
-        'CURLOPT_HTTPHEADER' => [
-            'Content-Type: application/json',
-        ],
-        'CURLOPT_TIMEOUT_MS' => 1000,       // cap total wait
-        'CURLOPT_CONNECTTIMEOUT' => 3,      // cap connection handshake
-        'CURLOPT_NOSIGNAL' => 1,
-    ]);
- 
-    $payload = json_encode([
-        'quizID' => $cmid,
-        'status' => '',
-        'createdOn' => gmdate('Y-m-d\TH:i:s.v\Z'),
-        'modifiedOn' => gmdate('Y-m-d\TH:i:s.v\Z'),
-        'studentCount' => 0,
-    ]);
-    
-    $response = $curl->post(
-        'https://proctoringlms.azurewebsites.net/api/Proctoring/StartQuiz',
-        $payload
-    );
+    public static function attempt_submitted(\mod_quiz\event\attempt_submitted $event) {
+        global $CFG, $DB;
+        require_once($CFG->libdir . '/filelib.php');
 
-    $record = new \stdClass();
-    $record->userid       = $userid;
-    $record->attemptid    = $attemptid;
-    $record->cmid         = $cmid;
-    $record->apiresponse     = json_encode($response, true);
-    $record->timecreated  = time();
-  
-    $insert = $DB->insert_record('local_proctoring_quiz_startattemptlog', $record);
+        $cmid = $event->contextinstanceid;
     
-}
+        $curl = new \curl();
+        $curl->setopt([
+            'CURLOPT_HTTPHEADER' => [
+                'Content-Type: application/json',
+            ],
+            'CURLOPT_TIMEOUT_MS' => 1000,       // cap total wait
+            'CURLOPT_CONNECTTIMEOUT' => 3,      // cap connection handshake
+            'CURLOPT_NOSIGNAL' => 1,
+        ]);
+    
+        $payload = json_encode([
+            'quizID' => $cmid,
+            'status' => '',
+            'createdOn' => gmdate('Y-m-d\TH:i:s.v\Z'),
+            'modifiedOn' => gmdate('Y-m-d\TH:i:s.v\Z'),
+            'studentCount' => 0,
+        ]);
+        
+        $response = $curl->post(
+            'https://proctoringlms.azurewebsites.net/api/Proctoring/StartQuiz',
+            $payload
+        );
+
+        $record = new \stdClass();
+        $record->userid       = $event->userid;
+        $record->attemptid    = $event->objectid;
+        $record->cmid         = $event->contextinstanceid;
+        $record->apiresponse     = json_encode($response, true);
+        $record->timecreated  = time();
+
+        $insert = $DB->insert_record('local_proctoring_quiz_startattemptlog', $record);
+        
+    }
 
 }
